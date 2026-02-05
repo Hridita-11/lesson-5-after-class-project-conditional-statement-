617315b13499db7cae4f675df955319c9e0300af
Hridita-11 <hridita.nz@gmail.com>
2026-02-05 20:25:02 +1300

    Add files via upload

---
+++ summertime_project.py
@@ -0,0 +1,7 @@
+temperature= int(input("Enter the temperature in C : " ))
+if temperature <15:
+    print("Very cold. Wear a jacket and pullover.")
+elif temperature<25:
+    print("Mild weather. Light jacket is enough.")
+else:
+    print("Hot weather. Wear light cloths.")
